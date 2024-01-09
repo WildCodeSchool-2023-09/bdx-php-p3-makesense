b9a1280f8ebc3b395cb12aed9acca49662ee6b00
Jojopok <zaiajonathan@gmail.com>
2024-01-09 11:02:30 +0100

    get dev

---
+++ src/Form/DecisionType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Decision;
+use App\Entity\Group;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DecisionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('status')
+            ->add('description')
+            ->add('impact')
+            ->add('context')
+            ->add('benefits')
+            ->add('risk')
+            ->add('startingDate')
+            ->add('deadlineOpinion')
+            ->add('deadlineDecision')
+            ->add('deadlineConflict')
+            ->add('deadlineFinal')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Decision::class,
+        ]);
+    }
+}
