90866312a6cb53fe32b4ff9f861bc91b2239a0aa
Jojopok <zaiajonathan@gmail.com>
2024-01-22 10:54:23 +0100

    fix bug

--- migrations/Version20240122094051.php
+++ migrations/Version20240122094051.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240116155319 extends AbstractMigration
+final class Version20240122094051 extends AbstractMigration
 {
     public function getDescription(): string
     {
