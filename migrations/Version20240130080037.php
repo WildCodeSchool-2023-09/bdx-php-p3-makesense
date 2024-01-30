7874f90f77632c05bf93239f3570c9be3750a5e7
KevinDavoust <kevin.davoust@orange.fr>
2024-01-30 09:00:42 +0100

    modified .env

--- migrations/Version20240130080037.php
+++ migrations/Version20240130080037.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240126133453 extends AbstractMigration
+final class Version20240130080037 extends AbstractMigration
 {
     public function getDescription(): string
     {
