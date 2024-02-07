8779d9dc6441aac463df8b7314447f4973956509
ComeSarazin <sasacoco@orange.fr>
2024-02-07 11:46:35 +0100

    finale version V1.1

--- migrations/Version20240207104436.php
+++ migrations/Version20240207104436.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240206160557 extends AbstractMigration
+final class Version20240207104436 extends AbstractMigration
 {
     public function getDescription(): string
     {
